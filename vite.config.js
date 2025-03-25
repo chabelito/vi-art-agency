bdfd124437dffa3d32ad5608040596c8a505b4c4
Varo <slashkanon@gmail.com>
2025-03-25 13:43:37 -0400

    Corregido base en vite.config.js y deploy exitoso

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/vi-art-agency/',
+  base: '/', // 👈 Usamos '/' porque es dominio raíz
   plugins: [react()],
 })
